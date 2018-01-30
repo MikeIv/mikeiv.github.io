3725d92c5a4bc46047f7df35d2d75fffc12330c5
MikeIv <mikeuivanov@gmail.com>
2018-01-30 16:43:14 +0300

    add vogoo

---
+++ VoGoo/mail.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?
+mail( "gagarahome@gmail.com", "VoGoo заказ", "Имя отправителя: $fio\nE-mail: $email\nОставил сообщение:\n$mess" );
+?>
+</body>
+</html>
